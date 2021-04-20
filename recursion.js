2f1a54e5d024ebedcd56ce9b5cd36668dae2ce97
Julia Ayres <j.bobrineva@gmail.com>
2021-04-20 18:07:57 -0400

    test passed for product function at recursion.js

---
+++ recursion.js
@@ -0,0 +1,55 @@
+/** product: calculate the product of an array of numbers. */
+
+function product (nums, i = 0) {
+	if (i === nums.length) return 1;
+	return nums[i] * product(nums, i + 1);
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest (words, i = 0) {
+	if (i === words.length) return;
+	let smallest = 0;
+	if (words[i].length > smallest) {
+		console.log(words[i].length);
+		smallest = words[i].length;
+		product(words, i + 1);
+	}
+	words[i].length;
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther (str) {}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome (str) {}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex (arr, val) {}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString (str) {}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings (obj) {}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch (arr, val) {}
+
+module.exports = {
+	product,
+	longest,
+	everyOther,
+	isPalindrome,
+	findIndex,
+	revString,
+	gatherStrings,
+	binarySearch
+};
